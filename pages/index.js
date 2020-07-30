361a931e28ca159a631cbfa928d1c4a6f7c6ac96
Roberto Loja <robertoloja@gmail.com>
2020-07-30 02:45:09 -0400

    Force deploy

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head"
 import stylesheet from 'styles/main.scss'
 
+
 import Header from "../components/Header"
 import Main from "../components/Main"
 import Footer from "../components/Footer"
