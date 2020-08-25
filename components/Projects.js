0e629b0e4a5b3c0b8d95f19196ef7f859b409f6f
Roberto Loja <robertoloja@gmail.com>
2020-08-24 21:22:46 -0400

    Simplify

--- components/Projects.js
+++ components/Projects.js
@@ -1,56 +1,27 @@
 import React from 'react'
-// import {resetIdCounter, Tab, TabList, TabPanel, Tabs} from 'react-tabs';
 import SmartHive from './Projects/SmartHive'
 import BookInventory from './Projects/BookInventory'
 import Other from './Projects/Other'
 import Website from './Projects/Website'
 import Tabs from './Tabs/Tabs'
-import Tab from './Tabs/Tab'
 
-const Projects = () => {
-    const tabStyle = {
-        borderTop: 'solid 1px',
-        borderRight: 'solid 1px',
-        float: 'left',
-        textAlign: 'center',
-        padding: '10px',
-        marginBottom: '-20px',
-        cursor: 'pointer',
-        transition: 'color .2s ease-in-out, background-color .2s ease-in-out,border-bottom-color .2s ease-in-out',
-    }
+const Projects = () =>
+    <Tabs>
+        <div label="SmartHive">
+            <SmartHive/>
+        </div>
 
-    const firstTabStyle = (() => {
-        let firstStyle = Object.assign({}, tabStyle)
-        firstStyle['borderRadius'] = '3px 0 0 0'
-        firstStyle['borderLeft'] = 'solid 1px'
-        return firstStyle
-    })()
+        <div label="Book Inventory">
+            <BookInventory/>
+        </div>
 
-    const lastTabStyle = (() => {
-        let lastStyle = Object.assign({}, tabStyle)
-        lastStyle['borderRadius'] = '0 3px 0 0'
-        return lastStyle
-    })()
+        <div label="This Website">
+            <Website/>
+        </div>
 
-    return (
-            <Tabs>
-                <div label="SmartHive">
-                    <SmartHive/>
-                </div>
-
-                <div label="Book Inventory">
-                    <BookInventory/>
-                </div>
-
-                <div label="This Website">
-                    <Website />
-                </div>
-
-                <div label="Other">
-                    <Other />
-                </div>
-            </Tabs>
-    )
-}
+        <div label="Other">
+            <Other/>
+        </div>
+    </Tabs>
 
 export default Projects
