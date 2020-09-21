2dc8ad997af118009b3fae2cfe5776ad9bb08fba
Roberto Loja <robertoloja@gmail.com>
2020-09-21 19:08:05 -0400

    Use DraftJS for text editor

--- components/Blog/TextEditor.js
+++ components/Blog/TextEditor.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import {Editor, EditorState} from 'draft-js';
+import 'draft-js/dist/Draft.css';
 
 const TextEditor = (props) => {
-    return <textarea defaultValue={props.text} style={{width: '80%', height: '200px'}}/>
+    const [editorState, setEditorState] = React.useState(
+        () => EditorState.createWithText(props.text),
+    );
+    return <Editor editorState={editorState} onChange={setEditorState} />
 }
 
 export default TextEditor
