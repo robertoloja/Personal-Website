cd46fbfa905ca76fc111d16a43ccf527c4c592da
Roberto Loja <robertoloja@gmail.com>
2020-09-22 17:56:39 -0400

    Add padding to more strongly signal that edit mode is on

--- components/Blog/TextEditor.js
+++ components/Blog/TextEditor.js
@@ -19,16 +19,17 @@ const TextEditor = (props) => {
     let value = (new Remarkable()).render(props.text)
 
     return (
-        <div style={{background: '#fff'}}>
+        <div style={{background: '#fff', padding: '10px'}}>
             <JoditEditor
                 ref={editor}
                 value={value}
                 config={config}
-                tabIndex={1} // tabIndex of textarea
-                onBlur={newContent => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
-                onChange={newContent => { Prism.highlightAll()
-                }}
-            /></div>)
+                tabIndex={1}
+                onBlur={newContent => setContent(newContent)}
+                onChange={Prism.highlightAll}
+            />
+        </div>
+    )
 }
 
 export default TextEditor
